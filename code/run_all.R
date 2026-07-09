61752795aabb6b5ecae4b450a1d827fe7c269629
Jiangyan Zhao <zhaojy2017@126.com>
2026-07-09 14:44:56 +0800

    add `run_all.R`

---
+++ code/run_all.R
@@ -0,0 +1,11 @@
+## Reproduce Section 4 examples
+
+source("code/s4_ex1_bkp_1d_logistic.R")
+source("code/s4_ex2_bkp_1d_nonlinear.R")
+source("code/s4_ex3_bkp_2d_goldstein_price.R")
+source("code/s4_ex4_bkp_two_spirals_classification.R")
+source("code/s4_ex5_dkp_1d_multinomial.R")
+source("code/s4_ex6_dkp_2d_multinomial.R")
+source("code/s4_ex7_dkp_iris_classification.R")
+source("code/s4_ex8_twinbkp_1d_nonlinear.R")
+source("code/s4_ex9_twindkp_1d_multinomial.R")
